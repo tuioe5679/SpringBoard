f293a3da4e84ea598bfcfc3f1313ffb3797e50e9
tuioe <qodlgks123@naver.com>
2022-07-19 16:14:18 +0900

    [Start] project Setting

---
+++ src/main/java/com/board/boardProject/BoardProjectApplication.java
@@ -0,0 +1,12 @@
+package com.board.boardProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardProjectApplication.class, args);
+	}
+}
