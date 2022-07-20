69836939278ebbf0cd3baf59378868adad6220f3
tuioe <qodlgks123@naver.com>
2022-07-20 16:00:22 +0900

    test: 엔티티 값 확인

--- src/main/java/com/board/boardProject/controller/BoardController.java
+++ src/main/java/com/board/boardProject/controller/BoardController.java
@@ -1,13 +1,22 @@
 package com.board.boardProject.controller;
 
+import com.board.boardProject.entity.Board;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BoardController {
 
     @GetMapping("/board/write")
-    public String boardWrite(){
+    public String boardWrite() {
         return "boardwrite";
     }
+
+    @PostMapping("/board/writepro")
+    public String boardWritePro(Board board){
+        System.out.println(board.getContent());
+        return "";
+    }
+
 }
