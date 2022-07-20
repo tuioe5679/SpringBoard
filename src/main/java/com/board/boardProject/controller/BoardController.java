74660c08c16edecaf49a81a62408eeee5a141346
tuioe <qodlgks123@naver.com>
2022-07-20 15:31:22 +0900

    feat: 게시물 작성 폼 추가

--- src/main/java/com/board/boardProject/controller/BoardController.java
+++ src/main/java/com/board/boardProject/controller/BoardController.java
@@ -2,18 +2,13 @@ package com.board.boardProject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-//REST API Controller를 사용할것을 프레임워크에 알려줌
 @Controller
 public class BoardController {
 
-    //HTTP GET 방식으로 문자열값을 매핑 (http://localhost:8080/Hello URL에 접속하면 메소드가 호출된다)
-    @GetMapping("/Hello")
-    //HTTP 요청에 Body로 변환하여 전달
-    @ResponseBody
-    public String main(){
-        return "Hello";
+    @GetMapping("/board/write")
+    public String boardWrite(){
+        return "boardwrite";
     }
 
 }
