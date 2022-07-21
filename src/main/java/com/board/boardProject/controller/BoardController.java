f23f2502ae049d2164e61379f89c97e379fa11bf
tuioe <qodlgks123@naver.com>
2022-07-21 15:03:25 +0900

    feat: 게시물 리스트 기능 추가

--- src/main/java/com/board/boardProject/controller/BoardController.java
+++ src/main/java/com/board/boardProject/controller/BoardController.java
@@ -4,11 +4,11 @@ import com.board.boardProject.entity.Board;
 import com.board.boardProject.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController//JSON 반환 어노테이션
+@Controller//JSON 반환 어노테이션
 public class BoardController {
 
     @Autowired
@@ -24,9 +24,16 @@ public class BoardController {
         return "boardwrite";
     }
 
+    @GetMapping("/board/list")
+    public String boardList(Model model){ //Model 데이터를 담아서 보냄
+        model.addAttribute("list",boardService.boardList());//"list"에 모든 정보를 저장
+        return "boardlist";
+    }
+
     @PostMapping("/board/writepro")
     public String boardWritePro(Board board){
         boardService.write(board);
         return "";
     }
+
 }
--- src/main/java/com/board/boardProject/service/BoardService.java
+++ src/main/java/com/board/boardProject/service/BoardService.java
@@ -5,6 +5,8 @@ import com.board.boardProject.repositroy.BoardRepositroy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BoardService {
 
@@ -13,5 +15,9 @@ public class BoardService {
 
     public void write(Board board){
         boardRepositroy.save(board);
+    }
+
+    public List<Board> boardList(){
+        return boardRepositroy.findAll();
     }
 }
